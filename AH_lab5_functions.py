44e451fd334113ed9fbca711b91caba3f200573c
avii123-byte <avishek.hamal@oregonstate.edu>
2025-05-13 18:50:08 -0700

    Updated the code using response from chatgpt

--- AH_lab5_functions.py
+++ AH_lab5_functions.py
@@ -1,14 +1,10 @@
 #####################
 # Block 1:  Import the packages you'll need
-# 
-# 
-
 import os, sys
 import rasterio
 import geopandas as gpd
-
-
-
+import numpy as np
+from rasterstats import zonal_stats  # this is needed for zonal statistics
 
 ##################
 # Block 2: 
@@ -16,7 +12,7 @@ import geopandas as gpd
 
 # Change this to the directory where your data are
 
-data_dir = "/Users/rkennedy/Dropbox/caol/me_as_professional/classes/2024_2025/geog4562/gis_data/2025"
+data_dir = r"R:\2025\Spring\GEOG562\Students\hamala\Labs\Lab5_2025"
 os.chdir(data_dir)
 print(os.getcwd())
 
@@ -26,14 +22,25 @@ print(os.getcwd())
 #   Set up a new smart raster class using rasterio  
 #    that will have a method called "calculate_ndvi"
 
+class SmartRaster:
+    def __init__(self, raster_path):
+        self.raster_path = raster_path
+        try:
+            self.dataset = rasterio.open(raster_path)
+        except Exception as e:
+            raise RuntimeError(f"Error opening raster file '{raster_path}': {e}")
 
-
-
-
-
-
-
-
+    def calculate_ndvi(self, red_band_index=4, nir_band_index=5):
+        try:
+            if red_band_index > self.dataset.count or nir_band_index > self.dataset.count:
+                raise ValueError("Specified band indices exceed number of bands in the raster.")
+            red = self.dataset.read(red_band_index).astype("float32")
+            nir = self.dataset.read(nir_band_index).astype("float32")
+            np.seterr(divide="ignore", invalid="ignore")
+            ndvi = (nir - red) / (nir + red)
+            return ndvi
+        except Exception as e:
+            raise RuntimeError(f"Error calculating NDVI for raster '{self.raster_path}': {e}")
 
 
 ##################
@@ -43,9 +50,17 @@ print(os.getcwd())
 #    to calculate the zonal statistics for a raster
 #    and add them as a column to the attribute table of the vector
 
+class SmartVector:
+    def __init__(self, vector_path):
+        self.vector_path = vector_path
+        try:
+            self.gdf = gpd.read_file(vector_path)
+        except Exception as e:
+            raise RuntimeError(f"Error loading vector file: {e}")
 
-
-
-
-
-
+    def calculate_zonal_statistics(self, raster_path, stats=['mean'], column_name='zonal_stat'):
+        try:
+            zs = zonal_stats(self.gdf, raster_path, stats=stats, geojson_out=True)
+            self.gdf[column_name] = [z[stats[0]] for z in zs]
+        except Exception as e:
+            raise RuntimeError(f"Error calculating zonal statistics: {e}")
--- AH_lab5_scripts.py
+++ AH_lab5_scripts.py
@@ -1,42 +1,45 @@
 # Lab 5 scripts
 
-import lab5_functions as l5
+import AH_lab5_functions as l5
+import matplotlib.pyplot as plt
+import geopandas as gpd
 
 #  Part 1:
 
 #  Assign a variable to the Landsat file 
-
+landsat_file = "Landsat_image_corv.tif"
 
 # Pass this to your new smart raster class
-
-
+smart_raster = l5.SmartRaster(landsat_file)
 
 # Calculate NDVI and save to and output file
-
-
-
-
-
+try:
+    ndvi = smart_raster.calculate_ndvi()
+    print("NDVI calculated successfully.")
+except Exception as e:
+    print(f"An error occurred while calculating NDVI: {e}")
 
 # Part 2:
 # Assign a variable to the parcels data shapefile path
-
+parcels_file = "Benton_County_TaxLots.shp"
 
 #  Pass this to your new smart vector class
-
+smart_vector = l5.SmartVector(parcels_file)
 
 #  Calculate zonal statistics and add to the attribute table of the parcels shapefile
-
-
+try:
+    smart_vector.calculate_zonal_statistics(landsat_file, stats=["mean"], column_name="mean_ndvi")
+    print("Zonal statistics calculated successfully.")
+    print(smart_vector.gdf.head())
+except Exception as e:
+    print(f"An error occurred while calculating zonal statistics: {e}")
 
 #  Part 3: Optional
 #  Use matplotlib to make a map of your census tracts with the average NDVI values
-
-
-
-
-
-
-
-
-
+try:
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    smart_vector.gdf.plot(column="mean_ndvi", ax=ax, legend=True, cmap="YlGn")
+    ax.set_title("Census Tracts with Average NDVI Values")
+    plt.show()
+except Exception as e:
+    print(f"An error occurred while plotting: {e}")
